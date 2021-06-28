d3a67fa7ba915b1ec5d6224da898da554957a2d5
mohnalkhateeb <mhmmd.alkateeb@gmail.com>
2021-06-28 17:10:07 +0300

    lab2f

--- src/components/HornedBeasts.js
+++ src/components/HornedBeasts.js
@@ -1,15 +1,46 @@
 import React from 'react'
-import reactDom from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card'
+
+// import reactDom from 'react-dom';
 
 class HornedBeasts extends React.Component
 {
+    constructor(props) {
+        super(props);
+        this.state = {
+            favorited: 0
+        }
+    }
+
+    favoritedplus = () => {
+        this.setState({
+            favorited: this.state.favorited + 1
+        })
+     
+        
+    }
     render() {
         return(
-            <div className="hornedBeasts">
-                <h2> {this.props.title}</h2> 
-                <img src= {this.props.source}  alt ={this.props.title}/>
-                <p> {this.props.detail}</p>
-            </div>
+            // <div className="hornedBeasts">
+            //     <h2> {this.props.title}</h2> 
+            //     <img onClick = {this.favoritedplus} src= {this.props.source}  alt ={this.props.title} width={250}/>
+            //     <p> {this.props.detail}</p>
+            //     <p> 💚{this.state.favorited}</p>
+            // </div>
+
+            <Card style={{ width: '250px' } } >
+                    <Card.Img onClick={this.favoritedplus} variant="top" src={this.props.source} />
+                    <Card.Body>
+                        <Card.Title>{this.props.Title}</Card.Title>
+                        <Card.Text>
+                        {this.props.detail}
+                        </Card.Text>
+                        <Card.Text>
+                        ❤️ {this.state.favorited}
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
         )
     }
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import reactDom from 'react-dom';
+// import reactDom from 'react-dom';
 import HornedBeasts from './HornedBeasts';
-class Main extends React.Component
-{
+import Horend from './Horend.json';
+
+// import { isTemplateElement } from '@babel/types';
+class Main extends React.Component {
     render() {
-        return(
-            <div>
-            <HornedBeasts title='Forest' source='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5omSUVfXJSD6O2yyNQ8yJveYS7Hb2Yll7Yw&usqp=CAU'  detail ='Here is the scary beauty and true fantasy'/>
-            <HornedBeasts title='dreset' source='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9j8N50X1BKfrDxai7-pVYHnTQqbjI0vL3EA&usqp=CAU'  detail ='The beauty of suffering and frightening calm'/>
-            <HornedBeasts title='sea' source='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7F9VbpOs4y0-6PQmx7EzMkfW8onuSi6iH8g&usqp=CAU'  detail ='The beauty of the unknown and the fear of the moment'/>
-    
+        return (
+            <div className='main'>
+
+                {
+                Horend.map(item => {
+                    return (<HornedBeasts title={item.title } source= {item.image_url} detail={item.description} />
+                        )
+                })
+                }
             </div>
         )
     }
