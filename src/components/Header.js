78c19a80a95cb599e9d6c7dec94813e260bbd0d7
mohnalkhateeb <mhmmd.alkateeb@gmail.com>
2021-06-28 10:45:38 +0300

    editeLab

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import reactDom from 'react-dom';
+
+class Header extends React.Component
+{
+    render() {
+        return(
+            <h1>Hello in My First React Code</h1>
+        )
+    }
+
+}
+
+export default Header
+
